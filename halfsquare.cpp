4df4346de9687dbca6c87514169a494b98ff0520
Jessica <jessicambromley7@gmail.com>
2022-05-29 16:22:48 -0400

    Update to display 5 lines

--- halfsquare.cpp
+++ halfsquare.cpp
@@ -3,8 +3,12 @@ using std::cout;
 using std::cin;
 
 int main () {
-    for(int i = 0; i <= 5; i++){
+
+    for(int lineNum = 0; lineNum < 5; lineNum++){
+        for(int hashNum = 0; hashNum <= 5; hashNum++){
         cout << "#";
+        }
+        cout << "\n";
     }
-    cout << "\n";
+    
 }
