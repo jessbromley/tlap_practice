d81c3a33304b6b42a916666a788c45a6ceb09ff5
Jessica <jessicambromley7@gmail.com>
2022-05-30 14:30:30 -0400

    Update to take digits as chars and convert to digit value to add to sum

--- luhn-checksum-val.cpp
+++ luhn-checksum-val.cpp
@@ -3,13 +3,18 @@ using std::cout;
 using std::cin;
 
 int main () {
-    //This function allows us to treat doubled numbers >= 10 as individual digits
-    int doubleDigitalValue(int digit) {
-        int doubledDigit = digit * 2;
-        int sum;
-        if (doubledDigit >= 10) sum = 1 + doubledDigit % 10;
-        else sum = doubledDigit;
-        return sum;
-    }
-    
+    //Takes a digit as a character and add digit value to sum
+    char digit;
+    cout << "Enter a one-digit number: ";
+    cin >> digit;
+    int sum = digit - '0';
+    cout << "Is the sum of digits " << sum << "? \n";
 }
+//This function allows us to treat doubled numbers >= 10 as individual digits
+int doubleDigitalValue(int digit) {
+    int doubledDigit = digit * 2;
+    int sum;
+    if (doubledDigit >= 10) sum = 1 + doubledDigit % 10;
+    else sum = doubledDigit;
+    return sum;
+    }
