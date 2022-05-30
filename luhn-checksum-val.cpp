4cf65989f9bd70a0cda69cf2b8e6abaed91562e3
Jessica <jessicambromley7@gmail.com>
2022-05-30 15:31:50 -0400

    Update to sum all numbers, doubling odd positions and ending when user clicks enter

--- luhn-checksum-val.cpp
+++ luhn-checksum-val.cpp
@@ -2,19 +2,35 @@
 using std::cout;
 using std::cin;
 
-int main () {
-    //Takes a digit as a character and add digit value to sum
-    char digit;
-    cout << "Enter a one-digit number: ";
-    cin >> digit;
-    int sum = digit - '0';
-    cout << "Is the sum of digits " << sum << "? \n";
-}
 //This function allows us to treat doubled numbers >= 10 as individual digits
-int doubleDigitalValue(int digit) {
+int doubleDigitValue(int digit) {
     int doubledDigit = digit * 2;
     int sum;
     if (doubledDigit >= 10) sum = 1 + doubledDigit % 10;
     else sum = doubledDigit;
     return sum;
     }
+
+int main () {
+    //Takes a digit as a char and adds digit value to sum, doubling odd position digits
+    char digit;
+    int checksum = 0;
+    int position = 1;
+    cout << "Enter a number with an even number of digits: ";
+    digit = cin.get();
+
+    while (digit != 10) {
+        if (position % 2 == 0) checksum += digit - '0';
+        else checksum += doubleDigitValue(digit - '0');
+        digit = cin.get();
+        position++;
+    }
+
+    cout << "Sum of digits is " << checksum << ". \n";
+    
+    if (checksum % 10 == 0) {
+        cout << "Checksum is divisible by 10. \n";
+    } else {
+        cout << "Checksum is not divisible by 10. Invalid. \n";
+    }
+}
