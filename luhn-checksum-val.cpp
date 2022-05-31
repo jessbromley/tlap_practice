eefb1d95d1f417b9d7aeae76aecb381d2a8705a8
Jessica <jessicambromley7@gmail.com>
2022-05-30 23:26:15 -0400

    Update final solution that takes ID number and validates based on luhn formula

--- luhn-checksum-val.cpp
+++ luhn-checksum-val.cpp
@@ -14,21 +14,31 @@ int doubleDigitValue(int digit) {
 int main () {
     //Takes a digit as a char and adds digit value to sum, doubling odd position digits
     char digit;
-    int checksum = 0;
+    int evenChecksum = 0;
+    int oddChecksum = 0;
     int position = 1;
-    cout << "Enter a number with an even number of digits: ";
+    cout << "Enter an ID number: ";
     digit = cin.get();
 
     while (digit != 10) {
-        if (position % 2 == 0) checksum += digit - '0';
-        else checksum += doubleDigitValue(digit - '0');
+        if (position % 2 == 0) {
+            evenChecksum += digit - '0';
+            oddChecksum += doubleDigitValue(digit - '0');
+        } else {
+            evenChecksum += doubleDigitValue(digit - '0');
+            oddChecksum += digit - '0';
+        }
         digit = cin.get();
         position++;
     }
-
-    cout << "Sum of digits is " << checksum << ". \n";
     
-    if (checksum % 10 == 0) {
+    if ((position - 1) % 2 == 0) {
+        cout << "Sum of digits is " << evenChecksum << ". \n";
+    } else {
+        cout << "Sum of digits is " << oddChecksum << ". \n";
+    }
+    
+    if (oddChecksum % 10 == 0) {
         cout << "Checksum is divisible by 10. \n";
     } else {
         cout << "Checksum is not divisible by 10. Invalid. \n";
