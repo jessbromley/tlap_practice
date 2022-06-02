09fc06f840b69852f671fa8598ca963f43e43dc6
Jessica <jessicambromley7@gmail.com>
2022-06-02 19:24:15 -0400

    Update solution to create downward arrow shape

--- hashDownArrow.cpp
+++ hashDownArrow.cpp
@@ -1,3 +1,19 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+
 int main () {
-    
+
+    for(int lineNum = 8; lineNum >= 1; lineNum -= 2){
+        for (int spaces = 0; spaces <= 9 - (lineNum+2); spaces+=2){
+            cout << " ";
+        }
+
+        for (int hashNum = 8; hashNum >= 9 - lineNum; hashNum--){
+                cout << "#";
+            }
+        
+        cout << "\n";
+    }
+
 }
